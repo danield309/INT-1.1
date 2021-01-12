58f78ed3aa6f2c07fcd38c2e8e8acac943e3c126
Daniel Duque <duque1daniel@gmail.com>
2021-01-11 20:18:02 -0800

    added voice assistant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import pyttsx3
+import speech_recognition as sr
+import re
 
 API_KEY = "ttOr9sRK0Yd5"
 PROJECT_TOKEN = "tFWNzeefNv9g"
@@ -47,5 +50,60 @@ class Data:
                 return content
             
             return "0"
-            
-data = Data(API_KEY, PROJECT_TOKEN)
+    
+    def get_list_of_countries(self): # Print list of countries with COVID cases
+        countries = []
+        for country in self.data['country']:
+            countries.append(country['name'].lower())
+        
+        return countries
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source: # Using mic from source send to said var and use google assistant to relay results
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+        except Exception as e:
+            print("Exception:", str(e)) # Return nothing if the assitant doesn't understand
+        
+    return said.lower()
+
+def main():
+	print("Started Program")
+	data = Data(API_KEY, PROJECT_TOKEN)
+	END_PHRASE = "stop"
+
+	TOTAL_PATTERNS = {
+					re.compile("[\w\s]+ total [\w\s]+ cases"):data.get_total_cases,
+					re.compile("[\w\s]+ total cases"): data.get_total_cases,
+                    re.compile("[\w\s]+ total [\w\s]+ deaths"): data.get_total_deaths,
+                    re.compile("[\w\s]+ total deaths"): data.get_total_deaths
+					}
+
+	while True:
+		print("Listening...")
+		text = get_audio()
+		print(text)
+		result = None
+
+		for pattern, func in TOTAL_PATTERNS.items():
+			if pattern.match(text):
+				result = func()
+				break
+
+		if result:
+			speak(result)
+
+		if text.find(END_PHRASE) != -1:  # Stop loop
+			print("Exit")
+			break
+
+main()

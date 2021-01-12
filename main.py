7e51e5122a6491b0f910b7c14c7cbc8582f590bc
Daniel Duque <duque1daniel@gmail.com>
2021-01-11 21:12:11 -0800

    debugged fix indentation issues

--- main.py
+++ main.py
@@ -3,90 +3,117 @@ import json
 import pyttsx3
 import speech_recognition as sr
 import re
+import threading
+import time
 
 API_KEY = "ttOr9sRK0Yd5"
 PROJECT_TOKEN = "tFWNzeefNv9g"
 RUN_TOKEN = "t-Uk8Ei-QRBe"
 
-# Authentication for GET request // This acquires the data I scraped using Parsehub 
-response = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key": API_KEY})
-data = json.loads(response.text)
-
-# Class that allows me to pull specific data from API
 class Data:
-    def __init__(self, api_key, project_token):
-        self.api_key = api_key
-        self.project_token = project_token
-        self.params = {
-            "api_key": self.api_key
-        }
-        self.get_data()
+	def __init__(self, api_key, project_token):
+		self.api_key = api_key
+		self.project_token = project_token
+		self.params = {
+			"api_key": self.api_key
+		}
+		self.data = self.get_data()
 
-    def get_data(self): # Method that can be called to update data
-        response = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key": API_KEY})
-        self.data = json.loads(response.text)
+	def get_data(self): # Method that can be called to update data
+		response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/last_ready_run/data', params=self.params)
+		data = json.loads(response.text)
+		return data
 
-    def get_total_cases(self):
-        data = self.data['total']
+	def get_total_cases(self):
+		data = self.data['total']
 
-        for content in data: # For loop that checks if the name is equal to coronavirus cases it will return the value
-            if content['name'] == "Coronavirus Cases:":
-                return content['value']
-    
-    def get_total_deaths(self):
-        data = self.data['total']
+		for content in data: # For loop that checks if the name is equal to coronavirus cases it will return the value
+			if content['name'] == "Coronavirus Cases:":
+				return content['value']
 
-        for content in data: # For loop that checks if the name is equal to coronavirus cases it will return the value
-            if content['name'] == "Deaths:":
-                return content['value']
-            
-            return "0"
-    
-    def get_country_data(self, country):
-        data = self.data["country"]
-        
-        for content in data: # For loop that checks if the name is equal to coronavirus deaths it will return the value
-            if content['name'].lower() == country.lower():
-                return content
-            
-            return "0"
-    
-    def get_list_of_countries(self): # Print list of countries with COVID cases
-        countries = []
-        for country in self.data['country']:
-            countries.append(country['name'].lower())
-        
-        return countries
+	def get_total_deaths(self):
+		data = self.data['total']
+
+		for content in data: # For loop that checks if the name is equal to coronavirus deaths it will return the value
+			if content['name'] == "Deaths:":
+				return content['value']
+
+		return "0"
+
+	def get_country_data(self, country):
+		data = self.data["country"]
+
+		for content in data:
+			if content['name'].lower() == country.lower():
+				return content
+
+		return "0"
+
+	def get_list_of_countries(self): # Print list of countries with COVID cases
+		countries = []
+		for country in self.data['country']:
+			countries.append(country['name'].lower())
+
+		return countries
+
+	def update_data(self):
+		response = requests.post(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/run', params=self.params)
+
+		def poll():
+			time.sleep(0.1)
+			old_data = self.data
+			while True:
+				new_data = self.get_data()
+				if new_data != old_data:
+					self.data = new_data
+					print("Data updated.") # Shows when data is finalized with an update
+					break
+				time.sleep(5)
+
+
+		t = threading.Thread(target=poll)
+		t.start()
+
 
 def speak(text):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
+	engine = pyttsx3.init()
+	engine.say(text)
+	engine.runAndWait()
+
 
 def get_audio():
-    r = sr.Recognizer()
-    with sr.Microphone() as source: # Using mic from source send to said var and use google assistant to relay results
-        audio = r.listen(source)
-        said = ""
+	r = sr.Recognizer()
+	with sr.Microphone() as source: # Using mic from source send to said var and use google assistant to relay results
+		audio = r.listen(source)
+		said = ""
 
-        try:
-            said = r.recognize_google(audio)
-        except Exception as e:
-            print("Exception:", str(e)) # Return nothing if the assitant doesn't understand
-        
-    return said.lower()
+		try:
+			said = r.recognize_google(audio)
+		except Exception as e:
+			print("Exception:", str(e)) # Return nothing if the assitant doesn't understand
+
+	return said.lower()
+
 
 def main():
-	print("Started Program")
+	print("Started Program") # Shows on program start
 	data = Data(API_KEY, PROJECT_TOKEN)
-	END_PHRASE = "stop"
+	END_PHRASE = "stop" # Use this word to stop the program 
+	country_list = data.get_list_of_countries()
 
-	TOTAL_PATTERNS = {
+	TOTAL_PATTERNS = { # voice patterns that will get replies for total questions
 					re.compile("[\w\s]+ total [\w\s]+ cases"):data.get_total_cases,
 					re.compile("[\w\s]+ total cases"): data.get_total_cases,
                     re.compile("[\w\s]+ total [\w\s]+ deaths"): data.get_total_deaths,
                     re.compile("[\w\s]+ total deaths"): data.get_total_deaths
 					}
+
+	COUNTRY_PATTERNS = { # voice patterns that will get replies for country specific questions
+					re.compile("[\w\s]+ cases [\w\s]+"): lambda country: data.get_country_data(country)['total_cases'],
+                    re.compile("[\w\s]+ deaths [\w\s]+"): lambda country: data.get_country_data(country)['total_deaths'],
+					}
+
+	UPDATE_COMMAND = "update" # update data command
 
 	while True:
 		print("Listening...")
@@ -94,10 +121,22 @@ def main():
 		print(text)
 		result = None
 
+		for pattern, func in COUNTRY_PATTERNS.items():
+			if pattern.match(text):
+				words = set(text.split(" "))
+				for country in country_list:
+					if country in words:
+						result = func(country)
+						break
+
 		for pattern, func in TOTAL_PATTERNS.items():
 			if pattern.match(text):
 				result = func()
 				break
+
+		if text == UPDATE_COMMAND: # Shows when data is being updated
+			result = "Data is being updated..!"
+			data.update_data()
 
 		if result:
 			speak(result)

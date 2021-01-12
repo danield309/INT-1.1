8fb592f86f0f9bff0ba27d6f3673326140b6011c
Daniel Duque <duque1daniel@gmail.com>
2021-01-11 18:48:19 -0800

    added API

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import requests
+import json
+
 API_KEY = "ttOr9sRK0Yd5"
 PROJECT_TOKEN = "tFWNzeefNv9g"
 RUN_TOKEN = "t-Uk8Ei-QRBe"
+
+# Authentication for GET request // This acquires the data I scraped using Parsehub 
+response = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key": API_KEY})
+data = json.loads(response.text)
